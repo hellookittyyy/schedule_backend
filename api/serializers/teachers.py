18ae50c63d82fec36425a37680c0d2f4b261d9a9
Kateryna Shapovalova <katya.shapovalova3012@gmail.com>
2026-02-03 11:41:01 +0200

    added oportunity for added subjets at the same time than creating new teacher

--- api/serializers/teachers.py
+++ api/serializers/teachers.py
@@ -8,4 +8,25 @@ class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
         fields = ['id', 'name', 'subjects', 'subjects_details']
-        extra_kwargs = {'subjects': {'write_only': True}}
+        extra_kwargs = {'subjects': {'write_only': True, 'required': False}}
+
+    def create(self, validated_data):
+        subjects = validated_data.pop('subjects', [])
+        teacher = Teacher.objects.create(**validated_data)
+        
+        if subjects:
+            teacher.subjects.set(subjects)
+        
+        return teacher
+
+    def update(self, instance, validated_data):
+        subjects = validated_data.pop('subjects', None)
+        
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        
+        if subjects is not None:
+            instance.subjects.set(subjects)
+        
+        return instance
